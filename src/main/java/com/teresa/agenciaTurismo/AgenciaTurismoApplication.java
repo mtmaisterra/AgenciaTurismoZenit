23eb6994744ec69da58cf16e2ecc7695480b8f34
Teresa <mtmaisterra@gmail.com>
2024-03-16 22:19:56 -0300

    primer commit

---
+++ src/main/java/com/teresa/agenciaTurismo/AgenciaTurismoApplication.java
@@ -0,0 +1,13 @@
+package com.teresa.agenciaTurismo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgenciaTurismoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgenciaTurismoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teresa/agenciaTurismo/AgenciaTurismoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teresa.agenciaTurismo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgenciaTurismoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
